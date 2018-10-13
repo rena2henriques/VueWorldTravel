903286d39462d4fc808696a5499d4efdf6e8fd77
rena2henriques <renato.manao@gmail.com>
2018-10-13 17:06:00 +0100

    First version of the World Travel Vue

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<template>
+	<div id="app">
+		
+		<span id="title">Show your <b>CONQUEST!</b></span>
+		<div id="mySelect">
+			<v-select label="name" :options="countries" v-model="selected" placeholder="Insert a country you have visited!"></v-select>
+			<button @click="addToVisited(selected)">Add</button>
+			<button @click="removeFromVisited(selected)">Remove</button>
+		</div>
+		<WorldMap :visitedCountries="visitedCountries"></WorldMap>
+	</div>
+</template>
+
+<script>
+import WorldMap from './components/WorldMap.vue';
+import vSelect from "vue-select";
+import Countries from "./countries.js";
+
+export default {
+	components: {
+		WorldMap,
+    "v-select": vSelect
+  },
+  data() {
+    return {
+      countries: Countries,
+			visitedCountries: {},
+      selected: null
+    };
+  },
+  methods: {
+		addToVisited(country) {
+			this.$set(this.visitedCountries,country.code, 500);
+
+			this.selected = null;
+		},
+		removeFromVisited(country) {
+			this.$delete(this.visitedCountries,country.code);
+
+			this.selected = null;
+		}
+	}
+}
+</script>
+
+<style>
+
+body {
+  background-color: skyblue;
+}
+
+#mySelect {
+	position: fixed;
+	top: 0;
+  right: 0;
+	width: 30%;
+}
+
+#title {
+	position: fixed;
+	top: 0;
+  left: 0;
+	font-style: italic;
+	font-size: 1.875em;
+}
+
+</style>
---
+++ src/components/WorldMap.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="worldmap">
+    <vue-world-map id="worldmap" v-bind:countryData="visitedCountries" v-bind:defaultCountryFillColor="map.defaultCountryFillColor" v-bind:highColor='map.highColor' v-bind:countryStrokeColor='map.countryStrokeColor' v-bind:lowColor="map.lowColor"></vue-world-map>
+  </div>
+</template>
+
+<script>
+import VueWorldMap from "vue-world-map";
+
+export default {
+  components: {
+    VueWorldMap
+  },
+  props: {
+    visitedCountries: Object
+  },
+  data() {
+    return {
+      map: {
+        defaultCountryFillColor: '#663300',
+        highColor: 'olive',
+        lowColor: 'olive',
+        countryStrokeColor: 'white'
+      }
+    };
+  }
+};
+</script>
+
+<style scoped>
+
+#worldmap .vue-world-map {
+  
+}
+
+
+
+</style>
